a63ce86f8046a93b600dda2be5f7dc4455b4f3d9
Imoni McCorvey <imccorv1@jhu.edu>
2016-05-29 10:59:04 -0400

    Intial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+    public float levelStartDelay = 2f;
+    public float turnDelay = 0.1f;
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+
+    private int level = 1;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
+	// Use this for initialization
+	void Awake () {
+    
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+	}
+
+    void OnLevelWasLoaded(int index)
+    {
+        level++;
+        InitGame();
+    }
+	void InitGame()
+    {
+        enemies.Clear();
+        boardScript.SetupScene(level);
+    }
+    public void GameOver()
+    {
+        //levelImage.SetActive(true);
+        enabled = false;
+    }
+	// Update is called once per frame
+	void Update () {
+        if (playersTurn || enemiesMoving)
+            return;
+        StartCoroutine(MoveEnemies());
+	}
+    public void AddEnemiesToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+        for(int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+        playersTurn = true;
+        enemiesMoving = false;
+    }
+}
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+    public Sprite dmgSprite;
+    public int hp = 1;
+
+    private SpriteRenderer spriteRenderer;
+	// Use this for initialization
+	void Awake () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	public void DamageWall(int less)
+    {
+        spriteRenderer.sprite = dmgSprite;
+        hp = -less;
+        if (hp <= 0)
+            gameObject.SetActive(false);
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
